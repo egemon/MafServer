52b292d7017b7e279019e92fd9a11743e2d69597
ilukianov <ilukianov@cogniance.com>
2015-12-10 20:09:51 +0200

    pertia; started

---
+++ routes/router.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var router = express.Router();
+var LocalGameStorage = require('../model/LocalGameStorage');
+/* GET home page. */
+router.get('/', function(req, res) {
+    console.log('res.url', res.url);
+    res.render('../public/views/home.ejs');
+});
+router.get('/home', function(req, res) {
+    console.log('req.url', req.url);
+    res.render('../public/views/home.ejs');
+});
+
+router.get('/about_us', function(req, res) {
+    console.log('req.url', req.url);
+    res.render('../public/views/home.ejs');
+});
+
+router.get('/news', function(req, res) {
+    res.render('../public/views/news.ejs');
+});
+
+router.get('/members', function(req, res) {
+    res.render('../public/views/members.ejs');
+});
+
+router.get('/rating', function(req, res) {
+    res.render('../public/views/rating.ejs');
+});
+
+router.get('/hall_of_fame', function(req, res) {
+    res.render('../public/views/hall_of_fame.ejs');
+});
+
+router.get('/friends', function(req, res) {
+    res.render('../public/views/friends.ejs');
+});
+
+// ================ handlers for MafTable ================ //
+
+//getGamesByFilter
+router.get('/sync', function (req, res) {
+    console.log('sync get request taken!');
+    var filterObject = req.query.filterObject;
+    console.log('req.data = ', filterObject);
+
+    var games = JSON.stringify(LocalGameStorage.getGamesByFilter(JSON.parse(filterObject)));
+    console.log('games = ', games);
+    res.send(games);
+});
+
+//getGamesByFilter
+router.post('/sync', function (req, res) {
+    console.log('psync post request taken!');
+    console.log('req.data = ', req.body.games);
+    var result = "";
+    if (LocalGameStorage.saveGameArray(JSON.parse(req.body.games))) {
+        result = 'Game Saved!';
+    } else {
+        result = 'There is no games!';
+    }
+    res.send(result);
+});
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var users = require('./routes/users');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(compress());
 app.use(favicon(__dirname + '/public/favicon.ico'));
