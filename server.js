12093570c45afb5a89f46173977a7ff29c194643
ilukianov <ilukianov@cogniance.com>
2015-12-10 19:38:24 +0200

    Project structure changed

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var routes = require('./routes/router');
 var users = require('./routes/users');
 
 
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public/')));
+app.use(express.static(path.join(__dirname, '/')));
 
 app.use('/', routes);
 // app.use('/users', users);
