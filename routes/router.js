e4c3f56d980c7705ec0e05cd14505b510145b36f
ilukianov <lykianovIB@gmail.com>
2015-12-22 10:30:38 +0200

    Hall of fame totally refactored

--- routes/router.js
+++ routes/router.js
@@ -1,15 +1,14 @@
+var honourTitle = "Лучшие игроки";
+var MONTHS = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"]
+var SEASONS = ["","ЗИМЫ", 'ВЕСНЫ', "ЛЕТА", "ОСЕНИ"]
+
 var express = require('express');
 var fs = require('fs');
 var router = express.Router();
 var LocalGameStorage = require('../model/LocalGameStorage');
 var photos = require('../data-base/photos.json');
-var contacts = require('../data-base/contacts.json');
-var periodsOfFame = require('../data-base/hall_of_fame/hall_of_fame.json');
 var players = require('../data-base/players/players.json');
 var meetingDefaults = require('../data-base/news/defaults.json');
-var famePlayers = periodsOfFame.players;
-
-periodsOfFame = sortFamePlayers(famePlayers);
 
 
 // ============ HELPERS ==============
@@ -28,22 +27,90 @@ function byHonourLevel (player1, player2) {
     return player1.memberLevel > player2.memberLevel;
 }
 
-function sortFamePlayers (players) {
-    for (var i = 0; i < players.length; i++) {
-
-    }
+// ==============  FOR hall_of_famЫe =================
+function createPeriodsOfFame (players) {
+    var honours = createHonours(players);
+    console.log('honours = ', honours);
+    var periods = createPeriods(honours);
+    console.log('periods = ', periods);
+    return periods.sort(byPeriod);
 }
 
-function derivePlayersByPeriodType (players) {
-    var result = {
-        year: [],
-        season: [],
-        month: []
+function createHonours (players) {
+    var player = {};
+    var honour = {};
+    var honours = [];
+    for (var i = 0; i < players.length; i++) {
+        player = players[i];
+        for (var j = 0; j < player.honours.length; j++) {
+            honour = player.honours[j];
+            honours.push({
+                nick: player.nick,
+                year: honour.year,
+                period: honour.period,
+                periodType: honour.periodType,
+                place: honour.place,
+                avr: honour.avr,
+                gameNumber: honour.gameNumber,
+                faculty: player.faculty,
+                experiance: player.experiance,
+                honours: player.honours || []
+                //other stuffn
+            });
+        };
     };
-    for (var i = 0; i < players.length; i++) {
+    return honours;
+}
 
+function createPeriods (honours) {
+    var periods = {};
+    var periodTitle = "";
+    var period = {};
+    var honour = {};
+    for (var i = 0; i < honours.length; i++) {
+        honour = honours[i];
+        periodTitle = createPeriodTitle(honours[i]); 
+        period = periods[periodTitle];
+        if (!period) {
+            period = periods[periodTitle] = {
+                honours: [],
+                year: honour.year,
+                periodType: honour.periodType,
+                period: honour.period,
+                priority: getPeriodPriority(honour.periodType, honour.period)
+            };
+        }
+        period.honours[honour.place - 1] = honour;
+    };
+    var periodsArray = [];
+    for (var title in periods) {
+        periods[title].title = title;
+        periodsArray.push(periods[title]);
+    };
+    return periodsArray;
+}
+
+function createPeriodTitle (honour) {
+    switch (honour.periodType) {
+        case "year":
+            return honourTitle + ' ' + honour.period + '-го года'
+        case "season":
+            return honourTitle + ' ' + SEASONS[honour.period]
+        case "month":
+            return honourTitle + ' ' + MONTHS[honour.period]
     }
 }
+
+function byPeriod (period1, period2) {
+    return (period1.year - period2.year) ||
+        (period1.periodType === "year") || 
+        (period1.priority - period2.priority)
+}
+
+function getPeriodPriority (periodType, period) {
+    return periodType === "season" ? period * 3 - 1 / 2 : period ;
+}
+
 
 var meetingData = meetingDefaults;
 meetingData.when = '2015-12-22T17:00';
@@ -66,7 +133,7 @@ var PAGES = [{
     },{
         page: 'hall_of_fame',
         rus: 'Зал Славы',
-        periods: periodsOfFame
+        periods: createPeriodsOfFame(players)
     },{
         page: 'photos',
         rus: 'Фото',
