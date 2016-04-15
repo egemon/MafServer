6fb5b9a2386b79417d5def742ede64922c2eb404
ilukianov <ilukianov@cogniance.com>
2016-04-15 15:22:52 +0300

    Gulp git hooks fully installed

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,10 @@ var gulp = require('gulp');
 var guppy = require('git-guppy')(gulp);
 var shell = require('gulp-shell');
 
-gulp.task('pre-push', guppy.src('pre-push', function(files, originArray, func) {
-    console.log('args = ', arguments);
-    return shell.task('scp -r  5649a2480c1e66bce900006c@bs-mafiaclub.rhcloud.com:app-root/runtime/repo/data-base/ ./',
-        {
-            verbouse: true
-        });
+gulp.task('fetch', shell.task('scp -r  5649a2480c1e66bce900006c@bs-mafiaclub.rhcloud.com:app-root/runtime/repo/data-base/ ./', {
+    verbose: true
+}));
+
+gulp.task('pre-push', ['fetch'] , guppy.src('pre-push', function(files, originArray, func) {
+    console.log('Doing nothing here args = ', arguments);
 }));
