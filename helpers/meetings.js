c376d7a8310689384345e879cc1a40326ea65028
ilukianov <ilukianov@cogniance.com>
2016-04-18 17:29:15 +0300

    Added current events to data base

--- helpers/meetings.js
+++ helpers/meetings.js
@@ -4,28 +4,31 @@ function applyDefaults (meetings) {
 	var meeting = {};
 	var result = [];
 	for (var i = 0; i < meetings.length; i++) {
-		meeting = meetings[i]
+		meeting = meetings[i];
 		for (var key in defaults) {
 			if (!meeting[key]) {
 				meeting[key] = defaults[key];
-			};
-		};
+			}
+		}
 		result.push(meeting);
-	};
+	}
 	return result;
 }
 
-function byNumber (meeting1, meeting2) {
-	return meeting1.number - meeting2.number;
+function byDate (meeting1, meeting2) {
+	var d1 = new Date(meeting1.when);
+	var d2 = new Date(meeting2.when);
+	return d2 - d1;
 }
 
 function sortMeetings (meetings) {
-	return meetings.sort(byNumber);
+	console.log('[meetings] sortMeetings()', meetings);
+	return meetings.sort(byDate);
 }
 
 function getMeetings(meetings) {
-	return sortMeetings(applyDefaults(meetings)) 	
-} 
+	return sortMeetings(applyDefaults(meetings));
+}
 module.exports = {
 	getMeetings: getMeetings
-}
+};
