72c006b94d66d69505f105b3c613e792873a4d35
ilukianov <ilukianov@cogniance.com>
2016-04-21 16:55:40 +0300

    Set limit for files to 1 mb

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ app.set('view engine', 'ejs');
 app.use(compress());
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '1mb'}));
+app.use(bodyParser.urlencoded({ extended: false, limit: '5mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/')));
 
