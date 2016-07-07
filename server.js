ba14fe76661280fb64d962fdfe92f94dd3dcf32f
ilukianov <lykianovIB@gmail.com>
2016-07-07 16:36:18 +0300

    [DO] Added npm install to npm start

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ module.exports = app;
 
 //  Set the environment variables we need.
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port      = process.env.PORT || 8080;
 
 if (typeof ipaddress === "undefined") {
     //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
